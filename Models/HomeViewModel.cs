ff4380121facf282ea2845f0ddf710877f709fa0
Felipekatao <cataofelipe@gmail.com>
2022-01-21 23:47:24 -0300

    update

---
+++ Models/HomeViewModel.cs
@@ -0,0 +1,9 @@
+namespace FOS.Models
+{
+    public class ListDelic
+    {
+        public string title {get;set;}
+        public int id {get;set;}
+        public bool completed {get;set;}
+    }
+}
